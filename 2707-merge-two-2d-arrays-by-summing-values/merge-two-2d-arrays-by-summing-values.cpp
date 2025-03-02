7306f842233abec931acb6c867b57e6e5d6a3e3c
Gaurav Jangid <110090153+gaur8av@users.noreply.github.com>
2025-03-02 08:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (64.62%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -4,32 +4,34 @@ public:
         int m = nums1.size();
         int n = nums2.size();
 
-        unordered_map<int, int> mp;
+        int i = 0;
+        int j = 0;
         vector<vector<int>> result;
-
-        for(int i = 0; i < m; i++) {
-            int id  = nums1[i][0];
-            int val = nums1[i][1];
-
-            mp[id] += val;
+        
+        while(i < m && j < n) {
+            if(nums1[i][0] < nums2[j][0]) {
+                result.push_back(nums1[i]);
+                i++;
+            } else if(nums2[j][0] < nums1[i][0]) {
+                result.push_back(nums2[j]);
+                j++;
+            } else {
+                result.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            }
         }
 
-        for(int i = 0; i < n; i++) {
-            int id  = nums2[i][0];
-            int val = nums2[i][1];
-
-            mp[id] += val;
+        while(i < m) {
+            result.push_back(nums1[i]);
+            i++;
         }
 
-        for(auto &it : mp) {
-            int key = it.first;
-            int val = it.second;
-
-            result.push_back({key, val});
+        while(j < n) {
+            result.push_back(nums2[j]);
+            j++;
         }
-
-        sort(begin(result), end(result)); //{id, val}
-
+        
         return result;
     }
 };
