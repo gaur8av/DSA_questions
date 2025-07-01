4225d87d812328898a483ac0e9562a0eccc149fc
Gaurav Jangid <110090153+gaur8av@users.noreply.github.com>
2025-07-01 13:16:27 +0530

    Time: 624 ms (11.49%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-select player_id ,first_login from
-(
-    select player_id , event_date as first_login,
-    row_number() over(partition by player_id order by event_date) as 'rnk'
-    from Activity
-) t
-where t.rnk = 1
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
